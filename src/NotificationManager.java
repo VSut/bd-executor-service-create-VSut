37e509a9f022c53ea7b8cafb322397fc245f2906
VSut <vaino.sutinen@gmail.com>
2024-08-14 20:43:07 -0500

    passes tests

--- src/NotificationManager.java
+++ src/NotificationManager.java
@@ -1,6 +1,8 @@
 
 
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class NotificationManager {
 
@@ -9,7 +11,11 @@ public class NotificationManager {
      * @param tasks List of NotificationTasks tasks to be run
      */
     public void sendSaleNotificationsOut(List<NotificationTask> tasks) {
-        //Implement this method
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for (NotificationTask task : tasks) {
+            executorService.submit(task);
+        }
+        executorService.shutdown();
     }
 
 }
